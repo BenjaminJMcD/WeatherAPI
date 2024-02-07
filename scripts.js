422e5ab72a21c45d40ad36d8e93e52b417f75834
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-02-07 13:32:12 -0600

    async function to fetch city info w argument

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 
+async function getWeather(location) {
+
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1b39ce7977d24e5bb6a183739240202&q=${location}`, {mode:'cors'})
+
+    
+
+    return console.log(response.json());
+};
+
+getWeather("kansas_city");
